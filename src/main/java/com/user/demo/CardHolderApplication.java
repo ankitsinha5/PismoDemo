0b66c5df293f726347381064cdac651ddd62d19a
ankitsinha5 <ankitsinha5@gmail.com>
2022-06-02 13:54:21 +0530

    Update CardHolderApplication.java

--- src/main/java/com/user/demo/CardHolderApplication.java
+++ src/main/java/com/user/demo/CardHolderApplication.java
@@ -13,9 +13,6 @@ import org.springframework.boot.autoconfigure.SpringBootApplication;
 
 @SpringBootApplication
 public class CardHolderApplication {
-	
-	@Autowired
-	//UserRepository userRepository;
 
 	public static void main(String[] args) {
 		SpringApplication app = new SpringApplication(CardHolderApplication.class);
